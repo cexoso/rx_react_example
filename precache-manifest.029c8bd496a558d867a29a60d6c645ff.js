48b36c15b4f6766a6014fcfb67728800cf98d399
xiongjie <285516193@qq.com>
2018-11-26 17:54:10 +0800

    Updates

---
+++ precache-manifest.029c8bd496a558d867a29a60d6c645ff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0221a0365e46e6bf28b5",
+    "url": "/rx_react_example/static/js/runtime~main.0221a036.js"
+  },
+  {
+    "revision": "ca1f308549f9b31621d0",
+    "url": "/rx_react_example/static/js/main.ca1f3085.chunk.js"
+  },
+  {
+    "revision": "5f7b76c3480c0cbee86e",
+    "url": "/rx_react_example/static/js/1.5f7b76c3.chunk.js"
+  },
+  {
+    "revision": "9885b717681fc62cd26a082698753806",
+    "url": "/rx_react_example/index.html"
+  }
+];
