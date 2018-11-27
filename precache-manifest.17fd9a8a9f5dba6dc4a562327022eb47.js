b3324bcf1ffadc5bd7830b2425ed758e5d3a7d44
xiongjie <285516193@qq.com>
2018-11-27 10:07:54 +0800

    Updates

---
+++ precache-manifest.17fd9a8a9f5dba6dc4a562327022eb47.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0221a0365e46e6bf28b5",
+    "url": "/rx_react_example/static/js/runtime~main.0221a036.js"
+  },
+  {
+    "revision": "31cd89c622de9b5691df",
+    "url": "/rx_react_example/static/js/main.31cd89c6.chunk.js"
+  },
+  {
+    "revision": "76e5e310229c83502362",
+    "url": "/rx_react_example/static/js/1.76e5e310.chunk.js"
+  },
+  {
+    "revision": "da49e350b971f4b37e39645e1c0de13b",
+    "url": "/rx_react_example/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rx_react_example/precache-manifest.1769b59030ece9eb9ab84ba2144ae60e.js"
+  "/rx_react_example/precache-manifest.17fd9a8a9f5dba6dc4a562327022eb47.js"
 );
 
 workbox.clientsClaim();
